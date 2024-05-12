dde5123fe6998c1cc8865065e29cb07edb63d6cd
Alvaro Iporre <alvaroiporremartinez@gmail.com>
2024-05-12 16:51:02 -0400

    Add trycatch to ProblemSolverController

--- app/Http/Controllers/Api/ProblemSolverController.php
+++ app/Http/Controllers/Api/ProblemSolverController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Services\ProblemSolverService;
+use Exception;
 use Illuminate\Http\Request;
 
 class ProblemSolverController extends Controller
@@ -11,9 +12,13 @@ class ProblemSolverController extends Controller
 
     public function solve(Request $request, $id)
     {
-        $solver = new ProblemSolverService();
-        $input = $request->input('input');
-        $output = $solver->solve($id, $input);
-        return response()->json(['output' => $output]);
+        try {
+            $solver = new ProblemSolverService();
+            $input = $request->input('input');
+            $output = $solver->solve($id, $input);
+            return response()->json(['output' => $output]);
+        } catch (Exception $e) {
+            return response()->json(['error' => 'Internal Error 500']);
+        }
     }
 }
